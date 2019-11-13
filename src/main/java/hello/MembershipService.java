b2b903638cb8571b5566c7955c102f7720a9f3cf
Julian Commissaris <jpcommissaris@gmail.com>
2019-11-13 11:45:29 -0800

    jc - changed javadoc and pom files

--- src/main/java/hello/MembershipService.java
+++ src/main/java/hello/MembershipService.java
@@ -5,18 +5,25 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 public interface MembershipService {
 
     /** is current logged in user a member but NOT an admin
+     * @param oAuth2AuthenticationToken oauth token 
+     * @return is current logged in user a member but NOT an admin of the github org?
      * of the github org */
     public boolean isMember(OAuth2AuthenticationToken oAuth2AuthenticationToken);
 
-    /** is current logged in user a member of the github org */
+    /** is current logged in user a member of the github org 
+     * @param oAuth2AuthenticationToken oauth token 
+     * @return is current logged in user a member and an admin of the github org?
+    */
     public boolean isAdmin(OAuth2AuthenticationToken oAuth2AuthenticationToken);
 
     /** is current logged in user a member or admin of the
+     * @param oAuth2AuthenticationToken oauth token 
+     * @return is current logged in user a member or an admin of the github org?
      * github org */
     default public boolean isMemberOrAdmin(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
         return isMember(oAuth2AuthenticationToken) || isAdmin(oAuth2AuthenticationToken);
     }
-
+    
     default public String role(OAuth2AuthenticationToken token) {
         if (token==null)
             return "Guest";
